c3c5aeb268d8a70fc601d9b5d3ec2b6f49ef31fd
Mário Salembe <linomario199010@gmail.com>
2025-08-12 16:50:43 +0100

    Finalizado base de dashboard

--- frontend/infrawatch/src/app/dashboard/page.tsx
+++ frontend/infrawatch/src/app/dashboard/page.tsx
@@ -15,6 +15,7 @@ import {
 } from "lucide-react";
 import React from "react";
 import { Badge } from "@/components/ui/badge";
+import Image from "next/image";
 
 interface IButtonCustom {
   children: React.ReactNode;
@@ -139,7 +140,30 @@ export default function Dashboard() {
         </div>
       </nav>
       <main>
-        <header className="sticky top-0 left-0 w-full h-16 border-b border-zinc-900 flex items-center justify-between px-5"></header>
+        <header className="sticky top-0 left-0 w-full h-16 border-b border-zinc-900 flex items-center justify-between px-7">
+          <div className="flex items-center gap-3">
+            <h1 className="text-white text-xl">Infra Watch</h1>
+            <p className="text-cyan-500">RCS ANGOLA</p>
+          </div>
+          <div className="flex items-center gap-2">
+            <div className="p-1 rounded-full bg-[#161616] border border-zinc-800 flex gap-3 items-center">
+              <Image
+                src={"/app/male.svg"}
+                width={100}
+                height={100}
+                alt="User Avatar"
+                className="rounded-full size-7"
+              />
+              <p className="pe-3 text-white">Mário Salembe</p>
+            </div>
+            <Button
+              size={"icon"}
+              className="rounded-full border border-zinc-800"
+            >
+              <Bolt size={18} className="text-white size-5" />
+            </Button>
+          </div>
+        </header>
       </main>
     </div>
   );
